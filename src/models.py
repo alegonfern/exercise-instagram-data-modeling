acbf8ea7e30ff6e4f705ae4fc26df0141b5623a1
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-10-07 04:37:05 +0000

    Resubido_GIT_Personal

--- src/models.py
+++ src/models.py
@@ -1,32 +1,63 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Usuario(Base):
+    __tablename__ = 'usuarios'
 
-    def to_dict(self):
-        return {}
+    id = Column(Integer, primary_key=True)
+    nombre_usuario = Column(String)
+    correo_electronico = Column(String)
+    nombre_completo = Column(String)
+    biografia = Column(String)
+    foto_perfil = Column(String)
+
+    publicaciones = relationship('Publicacion', back_populates='usuario')
+    comentarios = relationship('Comentario', back_populates='usuario')
+    me_gustas = relationship('MeGusta', back_populates='usuario')
+
+class Publicacion(Base):
+    __tablename__ = 'publicaciones'
+
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuarios.id'))
+    imagen = Column(String)
+    descripcion = Column(String)
+    fecha_publicacion = Column(DateTime)
+
+    usuario = relationship('Usuario', back_populates='publicaciones')
+    comentarios = relationship('Comentario', back_populates='publicacion')
+    me_gustas = relationship('MeGusta', back_populates='publicacion')
+
+class Comentario(Base):
+    __tablename__ = 'comentarios'
+
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuarios.id'))
+    publicacion_id = Column(Integer, ForeignKey('publicaciones.id'))
+    texto_comentario = Column(String)
+
+    usuario = relationship('Usuario', back_populates='comentarios')
+    publicacion = relationship('Publicacion', back_populates='comentarios')
+
+class MeGusta(Base):
+    __tablename__ = 'me_gustas'
+
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuarios.id'))
+    publicacion_id = Column(Integer, ForeignKey('publicaciones.id'))
+
+    usuario = relationship('Usuario', back_populates='me_gustas')
+    publicacion = relationship('Publicacion', back_populates='me_gustas')
+
+def to_dict(self):
+    return {}
 
 ## Draw from SQLAlchemy base
 try:
